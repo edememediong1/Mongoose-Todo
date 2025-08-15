a742f8fe8251f76ae2ae334e4ca7e7a478c97ff3
Emediong Bassey Edem <edememediong1@gmail.com>
2025-08-15 03:15:42 +0100

    feat: setup the app.js

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 const express = require('express')
 const cors = require('cors');
-const mongoose = require('mongoose');
+const connectDB = require('./config/db.js');
+const todoRoutes = require('./routes/todoRoutes.js')
 require('dotenv').config()
+
+
+const app = express()
+const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB
+connectDB();
+
+
+app.use(express.json()) //Middleware to parse JSON
+app.use(cors()) //allow cross-origin requests
+
+
+//Routes
+app.use('/api/todos', todoRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`)
+})
